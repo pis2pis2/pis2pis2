2a21a31616e948ba2c7c356aebab9400bc3cc481
pis2pis2 <stepa1390@inbox.ru>
2020-01-21 13:09:02 +0300

    Our 5 commit

--- example_lesson_3.py
+++ example_lesson_3.py
@@ -1,24 +1,18 @@
-f=open('Документ.txt')
-wordlist = [f.read()]
-punclist = [".",";",":","!","?","/","\\",",","#","@","$","&",")","(","\""]
-for punc in punclist:
-    for word in wordlist:
-        wordlist=[word.replace(punc,'') for word in wordlist]
-a=word.split()
-print(wordlist)
-print(a)
-f.close()
-
 # f=open('Документ.txt')
-# content=f.read()
-# puncList = [".",";",":","!","?","/","\\",",","#","@","$","&",")","(","\""]
-# for word in content:
-#     content=content.replace('punc','')
-# print(content)
-#
-# a='2'
-# print(a,type(a))
-# b=list(a)
-# print(b,type(b))
-# c=str(b)
-# print(c,type(c))
+# wordlist = [f.read()]
+# punclist = [".",";",":","!","?","/","\\",",","#","@","$","&",")","(","\""]
+# for punc in punclist:
+#     for word in wordlist:
+#         wordlist=[word.replace(punc,'') for word in wordlist]
+# a=word.split()
+# print(wordlist)
+# print(a)
+# f.close()
+
+f = open('Документ.txt')
+content = f.read()
+content = content.replace(".","").replace(",","").replace(";","").replace(":","").replace("!","").replace("?","").replace("/","").replace(")","").replace("(","").replace("-","").replace("«","").replace("»","").replace("—","")
+lst = content.split()
+lst_lower = list(map(lambda x: x.lower(), lst))
+print(lst_lower)
+
