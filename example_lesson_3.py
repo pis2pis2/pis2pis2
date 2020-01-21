23e2e0bcfcc3de605be8e9309dcbec1119bcf7ed
pis2pis2 <stepa1390@inbox.ru>
2020-01-21 17:28:46 +0300

    Our 5 commit

--- example_lesson_3.py
+++ example_lesson_3.py
@@ -14,12 +14,26 @@ content = f.read()
 content = content.replace(".","").replace(",","").replace(";","").replace(":","").replace("!","").replace("?","").replace("/","").replace(")","").replace("(","").replace("-","").replace("«","").replace("»","").replace("—","")
 lst = content.split()
 lst_lower = list(map(lambda x: x.lower(), lst))
-# print(lst_lower)
+
+import pymorphy2
+morph = pymorphy2.MorphAnalyzer()
+lst_morph = []
+for word in lst_lower:
+    p = morph.parse(word)[0]
+    lst_morph.append(p.normal_form)
+
+lst_diff = set(lst_morph)
 
 dct = {}
-for word in lst_lower:
+for word in lst_morph:
     if word in dct:
         dct[word] +=1
     else:
         dct[word] = 1
 print(dct)
+
+print ('5 наиболее часто встречающихся слов:')
+lst_srt = sorted(dct.items(), key=lambda x: x[1],reverse=True)
+for i in range(5):
+    print(lst_srt[i][0],'-',lst_srt[i][1])
+print('Количество разных слов в тексте: ', len(lst_diff))
